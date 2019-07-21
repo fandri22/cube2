ed64142e493452d60254f5dab6e71bfc4da14bda
fandri22 <52689800+fandri22@users.noreply.github.com>
2019-07-22 05:56:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$user_agent = "com.breakcube.bc_1.1.15_17(Redmi 5;Android 7.1.2_25;in-ID)";
+
+//login
+$platform = "Android";
+$foreign_key = "bde4f60b9f6fb0b4";
+$sign = "6ed7cec3ab0cb7b76e7fd700c675d0d60f521429";
+$uuid = "2e690eaa8d354a1c5c9cd70816f40e1dd7bc6c6c";
+$aid = "0a93e810-132b-411f-9630-9845d641c346";
+$android_id = "bde4f60b9f6fb0b4";
+
+//click
+$setting_version = "8";
